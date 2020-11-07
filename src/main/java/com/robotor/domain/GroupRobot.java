39d404804f736601757cb73396bc0f2e70a31449
Thien VI CHi <meomeocf98@gmail.com>
2020-11-08 00:22:02 +0700

    complete:: GroupRobot

--- src/main/java/com/robotor/domain/GroupRobot.java
+++ src/main/java/com/robotor/domain/GroupRobot.java
@@ -1,11 +1,27 @@
 package com.robotor.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 public class GroupRobot {
+    private double distance = 0;
     private final List<Robot> robots;
 
     public GroupRobot(List<Robot> robots) {
         this.robots = robots;
+    }
+
+    GroupRobot go(double distance) {
+        this.distance += distance;
+        return this;
+    }
+
+    GroupRobot printStatus() {
+        robots.forEach(Robot::printInformation);
+        return this;
+    }
+
+    Optional<Robot> getEnergyConsumptionHighest() {
+        return robots.stream().max((robot1, robot2) -> robot1.getEnergyConsumption(distance) > robot1.getEnergyConsumption(distance) ? 1 : 0);
     }
 }
