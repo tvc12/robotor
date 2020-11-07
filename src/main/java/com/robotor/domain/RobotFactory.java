7baeabd8e8f56ed58cae76e059101e47e0d4b8fb
Thien VI CHi <meomeocf98@gmail.com>
2020-11-08 00:12:36 +0700

    add: RobotFactory and RobotFactoryBuilder

--- src/main/java/com/robotor/domain/RobotFactory.java
+++ src/main/java/com/robotor/domain/RobotFactory.java
@@ -1,16 +1,52 @@
 package com.robotor.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RobotFactory {
-    private List<Robot> robots;
+    private final List<Robot> robots;
+    private final Map<Class, RobotBuilderEngine> engineAsMap;
 
-    public <T extends Robot> RobotFactory create(Class<T> clazz, int quality) {
-//        throw new Exception();
+    public RobotFactory(Map<Class, RobotBuilderEngine> engineAsMap) {
+        this.engineAsMap = engineAsMap;
+        robots = new ArrayList<>();
+    }
+
+    public <T extends Robot> RobotFactory create(Class<T> clazz, int quality) throws Exception {
+        RobotBuilderEngine engine = engineAsMap.get(clazz);
+        if (engine != null) {
+            List newRobots = engine.create(quality);
+            this.robots.addAll(newRobots);
+        } else {
+            throw new Exception(String.format("class:: %s can not create at this time", clazz));
+        }
         return this;
     }
 
     public GroupRobot build() {
         return new GroupRobot(robots);
+    }
+
+    public static RobotFactoryBuilder builder() {
+        return new RobotFactoryBuilder();
+    }
+}
+
+class RobotFactoryBuilder {
+    private final Map<Class, RobotBuilderEngine> engineAsMap;
+
+    public RobotFactoryBuilder() {
+        this.engineAsMap = new HashMap<>();
+    }
+
+    public RobotFactoryBuilder addEngine(Class clazz, RobotBuilderEngine engine) {
+        this.engineAsMap.put(clazz, engine);
+        return this;
+    }
+
+    public RobotFactory build() {
+        return new RobotFactory(this.engineAsMap);
     }
 }
